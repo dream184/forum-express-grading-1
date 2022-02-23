eddeb1b8b4bb736ffa6e8d239744a7e7e8dbaf27
shibao <dream0184@yahoo.com.tw>
2022-02-23 16:11:32 +0800

    feat:add api-auth middleware

--- controllers/apis/admin-controller.js
+++ controllers/apis/admin-controller.js
@@ -3,6 +3,12 @@ const adminServices = require('../../services/admin-services')
 const adminController = {
   getRestaurants: (req, res, next) => {
     adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
+  },
+  deleteRestaurant: (req, res, next) => {
+    adminServices.deleteRestaurant(req, (err, data) => err ? next(err) : res.json(data))
+  },
+  postRestaurant: (req, res, next) => {
+    adminServices.postRestaurant(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 module.exports = adminController
--- routes/apis/modules/admin.js
+++ routes/apis/modules/admin.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../../controllers/apis/admin-controller')
+const upload = require('../../../middleware/multer')
+
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 module.exports = router
